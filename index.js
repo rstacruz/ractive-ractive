1a3ae55154340214484f260834e4d75e1eae0e5e
Rico Sta. Cruz <hi@ricostacruz.com>
2014-09-24 12:50:54 +0800

    More refactor.

--- index.js
+++ index.js
@@ -24,12 +24,17 @@
   Adaptor.fireWrapEvents = true;
   Adaptor.maxKeyLength = 2048;
 
+  /*
+   * Check if the child is an Ractive instance.
+   *
+   * Also, if this key has been wrapped before, don't rewrap it. (Happens on
+   * deeply-nested values, and .reset() for some reason.)
+   */
+
   function filter (child, keypath, parent) {
     if (!(child instanceof Ractive))
       return false;
 
-    // If this key has been wrapped before, don't rewrap it. This can happen on
-    // deeply-nested values, and .reset() for some reason.
     if (parent &&
         parent._ractiveWraps &&
         parent._ractiveWraps[keypath])
@@ -50,11 +55,8 @@
     };
 
     function setup () {
-      checkKeypath();
-
-      // Let future wrappers know what we have wrapped Ractive instances.
-      if (!parent._ractiveWraps) parent._ractiveWraps = {};
-      parent._ractiveWraps[keypath] = child;
+      checkForRecursion();
+      markAsWrapped();
 
       // If the child has its own Ractive instances, recurse upwards.
       // This will do `parent.set('child.grandchild', instance)` so that
@@ -64,7 +66,6 @@
       // Propagate child changes to parent.
       child.on('change', observer);
 
-      // Fire wrap events
       if (Adaptor.fireWrapEvents) {
         child.fire('wrap', parent, keypath);
         parent.fire('wrapchild', child, keypath);
@@ -75,7 +76,6 @@
       delete parent._ractiveWraps[keypath];
       child.off('change', observer);
 
-      // Fire wrap events
       if (Adaptor.fireWrapEvents) {
         child.fire('unwrap', parent, keypath);
         parent.fire('unwrapchild', child, keypath);
@@ -100,11 +100,14 @@
       pause = false;
     }
 
+    /*
+     * Allow setting values by passing a POJO to .set(), for instance,
+     * `.set('child', { ... })`. If anything else is passed onto .set()
+     * (like another Ractive instance, or another adaptor'able), destroy
+     * this wrapper.
+     */
+
     function reset (object) {
-      // Allow setting values by passing a POJO to .set(), for instance,
-      // `.set('child', { ... })`. If anything else is passed onto .set()
-      // (like another Ractive instance, or another adaptor'able), destroy
-      // this wrapper.
       if (object && object.constructor === Object) {
         child.set(object);
       } else {
@@ -112,10 +115,24 @@
       }
     }
 
-    // keypath will look like 'child.sub.parent.child.sub.parent' ad nauseum.
-    function checkKeypath () {
+    /*
+     * Die on recursion.
+     * Keypath will look like 'child.sub.parent.child.sub.parent' ad nauseum.
+     */
+
+    function checkForRecursion () {
       if (keypath && keypath.length > Adaptor.maxKeyLength)
         throw new Error("Keypath too long (possible circular dependency)");
+    }
+
+    /*
+     * Let future wrappers know what we have wrapped Ractive instances.
+     * This value is used on `filter()`.
+     */
+
+    function markAsWrapped () {
+      if (!parent._ractiveWraps) parent._ractiveWraps = {};
+      parent._ractiveWraps[keypath] = child;
     }
   }
 
