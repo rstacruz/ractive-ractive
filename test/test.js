b959f36279c18073c1c55b5eeab6ccc187a89243
Rico Sta. Cruz <hi@ricostacruz.com>
2014-10-01 14:02:54 +0800

    Add tests for when it's used via Ractive.defaults.

--- test/test.js
+++ test/test.js
@@ -368,6 +368,41 @@ describe('computed properties', function () {
   });
 });
 
+describe('Ractive.defaults', function () {
+  var one, two, audio;
+
+  beforeEach(function () {
+    audio = new Ractive({ data: {
+      volume: 30,
+      mute: true
+    }});
+
+    Ractive.defaults.data.audio = audio;
+    one = new Ractive();
+    two = new Ractive();
+  });
+
+  it('picks up shared objects from the start', function () {
+    expect(one.get('audio.volume')).eql(30);
+    expect(two.get('audio.volume')).eql(30);
+    expect(audio.get('volume')).eql(30);
+  });
+
+  it('picks up changes from the shared object', function () {
+    audio.set('volume', 60);
+    expect(one.get('audio.volume')).eql(60);
+    expect(two.get('audio.volume')).eql(60);
+    expect(audio.get('volume')).eql(60);
+  });
+
+  xit('picks up changes from the shared object', function () {
+    one.set('audio.volume', 70);
+    expect(one.get('audio.volume')).eql(70);
+    expect(two.get('audio.volume')).eql(70);
+    expect(audio.get('volume')).eql(70);
+  });
+});
+
 afterEach(function expectLocksReleased() {
   expect(Object.keys(adapt.locked)).length(0);
 });
