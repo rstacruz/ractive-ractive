3c63d16e6c3008988c28089b599d24df156d0aa0
Rico Sta. Cruz <hi@ricostacruz.com>
2014-10-01 23:02:42 +0800

    Update mocha-multi for splatting.

--- test/mocha-multi.js
+++ test/mocha-multi.js
@@ -1,6 +1,6 @@
 /**
  * mdescribe() : mdescribe(name, specs, fn)
- * makes multiple `describe` blocks that iterates through given `specs`.
+ * Wakes multiple `describe` blocks that iterates through given `specs`.
  *
  *     stubs = {
  *       'jquery-1.9': require('../vendor/jquery-1.9.js'),
@@ -11,13 +11,30 @@
  *     mdescribe("Tests", stubs, function (jquery) {
  *       // tests here. `jquery` will be the values of stubs
  *     })
+ *
+ * If the `specs` has array values, they will be spread through the function
+ * args:
+ *
+ *     stubs = {
+ *       'bb1.0 + u1.2': [ backbone['1.0'], underscore['1.2'] ],
+ *       'bb1.0 + u1.3': [ backbone['1.0'], underscore['1.3'] ],
+ *       'bb1.0 + u1.4': [ backbone['1.0'], underscore['1.4'] ],
+ *       'bb1.3 + u1.2': [ backbone['1.3'], underscore['1.2'] ],
+ *       'bb1.3 + u1.3': [ backbone['1.3'], underscore['1.3'] ],
+ *       'bb1.3 + u1.4': [ backbone['1.3'], underscore['1.4'] ],
+ *     }
+ *
+ *     mdescribe("Tests", stubs, function (Backbone, _) {
+ *     });
  */
 
 global.mdescribe = function (name, specs, fn) {
   Object.keys(specs).forEach(function (key) {
     var item = specs[key];
     describe(name + " (" + key + ")", function () {
-      return fn.call(this, item, key);
+      var args = Array.isArray(item) ? item : [ item ];
+      args.push(key);
+      return fn.apply(this, args);
     });
   });
 };
